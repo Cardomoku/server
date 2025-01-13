7aebbba2c10bc3b7ae839eee0c1eadba9b8470e4
정재웅 <jweidon0821@gmail.com>
2025-01-13 23:56:11 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/jjweidon/cardomoku/CardomokuApplication.java
@@ -0,0 +1,13 @@
+package com.jjweidon.cardomoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardomokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardomokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjweidon/cardomoku/CardomokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjweidon.cardomoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardomokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
